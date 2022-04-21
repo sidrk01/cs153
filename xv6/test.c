c72c7131b2acb4c62b9a82c8c3ba12f0355ca0cb
Sidharth Ramkumar <sramk002@ucr.edu>
2022-04-21 16:00:41 -0700

    part 1

---
+++ xv6/test.c
@@ -0,0 +1,12 @@
+//
+// Created by Sidharth Ramkumar on 4/17/22.
+//
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    //printf(1, "hello world\n");
+    hello(); //J.H
+    exit(1);
+}
