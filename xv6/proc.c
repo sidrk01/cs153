377297deb5ab3c376d0188392b370a63a3b203e4
Sidharth Ramkumar <sramk002@ucr.edu>
2022-04-22 09:40:34 -0700

    part 3

--- xv6/proc.c
+++ xv6/proc.c
@@ -273,7 +273,7 @@ int
 wait(int* status)
 {
   struct proc *p;
-  int havekids, pid;
+  int havekids, pid;// pid;
   struct proc *curproc = myproc();
   
   acquire(&ptable.lock);
@@ -312,6 +312,51 @@ wait(int* status)
     // Wait for children to exit.  (See wakeup1 call in proc_exit.)
     sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
+}
+
+int
+wait(int, pid, int* status, int options)
+{
+    struct proc *p;
+    int iscurr;
+    struct proc *curproc = myproc();
+
+    acquire(&ptable.lock);
+    for(;;){
+        // Scan through table looking for exited children.
+        iscurr = 0;
+        for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+            if(p->pid != pid)
+                continue;
+            havekids = 1;
+            if(p->state == ZOMBIE){
+                // Found one
+                pid = p->pid;
+                kfree(p->kstack);
+                p->kstack = 0;
+                freevm(p->pgdir);
+                p->pid = 0;
+                p->parent = 0;
+                p->name[0] = 0;
+                p->killed = 0;
+                p->state = UNUSED;
+                release(&ptable.lock);
+                if (*status != 0){
+                    *status = p->status;
+                }
+                return pid;
+            }
+        }
+
+        // No point waiting if we don't have any children.
+        if(!havekids || curproc->killed){
+            release(&ptable.lock);
+            return -1;
+        }
+
+        // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+        sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+    }
 }
 
 //PAGEBREAK: 42
